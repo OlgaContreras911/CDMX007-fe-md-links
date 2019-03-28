d759251bbee934a0ca4845dff5844253c0a721b4
OlgaContreras911 <rockersita.911@gmail.com>
2019-03-28 16:48:49 -0600

    agrega lectura de archivo md

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
+// module.exports = () => {
   // ...
-};
+// };
+
+const fs= require("fs");
+
+fs.readFile("./README.md", function (err,data)  {
+    if(err){
+
+    }
+    console.log(data.toString());
+})
