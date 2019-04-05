7a39cab3ec2284037e7b32f3dd7a2d1b8cab148e
OlgaContreras911 <rockersita.911@gmail.com>
2019-04-05 01:47:57 -0600

    agrega links en fetch

--- fetch.js
+++ fetch.js
@@ -1,16 +1,21 @@
 const fetch = require('node-fetch');
 const colors = require('colors');
+const path = require('path');
 
 const fetchData = (newMds)=>{
-newMds.forEach(element => {
-    fetch(element).then(res =>{
-        const status = res.status
-        if(status === 200){
-            console.log(`link: ${colors.blue(element)} status: ${colors.green(status)} OK`)
-        }else{
-        console.log(`link: ${colors.blue(element)} status: ${colors.red(status)} FAIL`)}
-    })
-    
-});
-}
+    newMds.forEach(element => {
+        const filePar = path.parse (element);
+        //console.log("filePar",filePar)
+            const linkName= filePar.name
+        fetch(element).then(res =>{
+            
+            const status = res.status
+            if(status === 200){
+                console.log(`Link name: ${colors.white(linkName)} link: ${colors.blue(element)} status: ${colors.green(status)} OK`)
+            }else if(status === 404){
+            console.log(`Link name: ${colors.white (linkName)} link: ${colors.blue(element)} status: ${colors.red(status)} FAIL`)}
+        })
+        
+    });
+    }
 module.exports.fetchData = fetchData
